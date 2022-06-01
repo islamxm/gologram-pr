1566ed23b3b386ce47cffe61352c8f3b965b70d0
Ислам Мадаминов <isxmad@gmail.com>
2022-06-01 18:04:14 +0300

    fix endpoint

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -1,5 +1,5 @@
 export class Endpoints  {
-    BASE_DOMAIN = '77.222.42.174/web';
+    BASE_DOMAIN = '77.222.42.174';
     signin = `http://${this.BASE_DOMAIN}/api/v1/auth/users/`;
     login = `http://${this.BASE_DOMAIN}/api/v1/auth/token/login/`;
     logout = `http://${this.BASE_DOMAIN}/api/v1/auth/token/login/`
